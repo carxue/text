2c808cb20835326378303ff5423f53671a48f156
carxue <xuekui_it@163.com>
2019-04-20 10:18:37 +0800

    提交修改

--- src/com/annotation/fieldconver/ObjectConvertUtils.java
+++ src/com/annotation/fieldconver/ObjectConvertUtils.java
@@ -1,10 +1,12 @@
 package com.annotation.fieldconver;
 
 import java.lang.reflect.Field;
-import java.util.Date;
+import java.math.BigDecimal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import com.annotation.fieldconver.FieldConvert.Ignore;
-import com.google.gson.Gson;
 
 /**
  * 将一个对象的值转换到另一个对象的对应属性上
@@ -13,7 +15,9 @@ import com.google.gson.Gson;
  */
 public class ObjectConvertUtils<T,K> {
 	
-	public static <T,K> void getSettingFields(T t, K k) {
+	private static Log logger = LogFactory.getLog(ObjectConvertUtils.class.getName());
+	
+	public static <T,K> void convertObjectAttrs(T t, K k) {
 		Class<?> toClass = t.getClass();
 		Field[] toFileds = null;
 		for (; toClass != Object.class; toClass = toClass.getSuperclass()) {
@@ -22,7 +26,7 @@ public class ObjectConvertUtils<T,K> {
 		}
 	}
 
-	public static <T,K> void loopSettingFields(T t, K k, Field[] toFileds) {
+	private static <T,K> void loopSettingFields(T t, K k, Field[] toFileds) {
 		for (Field field : toFileds) {
 			FieldConvert fieldAnno = field.getAnnotation(FieldConvert.class);
 			if (fieldAnno != null && fieldAnno.ignore() == Ignore.N) {
@@ -33,7 +37,7 @@ public class ObjectConvertUtils<T,K> {
 		}
 	}
 
-	public static <T,K> void settingToFileds(Field field, String filedName,T t, K k) {
+	private static <T,K> void settingToFileds(Field field, String filedName,T t, K k) {
 		Class<?> fromClass = k.getClass();
 		for (; fromClass != Object.class; fromClass = fromClass.getSuperclass()) {
 			try {
@@ -62,37 +66,16 @@ public class ObjectConvertUtils<T,K> {
 						field.set(t, Byte.parseByte(val));
 					}else if(type.equals(Boolean.class)){
 						field.set(t, Boolean.parseBoolean(val));
+					}else if(type.equals(BigDecimal.class)){
+						field.set(t, new BigDecimal(val.toString()));
 					}
 				}
 				break;
-			} catch (IllegalArgumentException e) {System.out.println(e.getMessage());}
-			 catch (Exception e) {System.out.println(e.getMessage());}
-		}
-	}
-	
-	public static void main(String[] args) {
-		User user = new User();
-		user.setName("薛奎");
-		user.setPsword("111111");
-		user.setAge(9100);
-		user.setTime(new Date());
-		user.setSex(Boolean.TRUE);
-		user.setMyCountry("中国");
-		
-		Person person = new Person();
-		ObjectConvertUtils.getSettingFields(person,user);
-		Gson gson = new Gson();
-		System.out.println(gson.toJson(person));
-		
-		
-		for(int i=0;i<args.length;i++){
-			System.out.println(args[i]);
+			} catch (IllegalArgumentException e){
+				logger.error("参数非法异常:"+e.getMessage());
+			}catch (Exception e){
+				logger.error("数据赋值异常:"+e.getMessage());
 			}
-		
-//		System.out.println(System.getProperty("mian.args"));
-//		System.out.println(System.getProperty("sun.boot.class.path"));
-//		System.out.println(System.getProperty("java.ext.dirs"));
-//		System.out.println(System.getProperty("java.class.path"));
-		
+		}
 	}
 }
