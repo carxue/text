ba70121a18eb3a6f50111f940de72beef40c35e8
carlxue2 <xuekui_it@163.com>
2020-09-04 17:45:44 +0800

    代码提交

--- src/com/annotation/test/Student.java
+++ src/com/annotation/test/Student.java
@@ -21,5 +21,10 @@ public class Student {
 	public void setAge(Integer age) {
 		this.age = age;
 	}
-	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", getName()=" + getName() + ", getAge()=" + getAge()
+				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
 }
